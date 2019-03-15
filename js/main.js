317ee82601c4a7d248241750a83bf3ac9b30f256
dragance023 <dragance023@outlook.com>
2019-03-15 09:17:16 +0100

    added some parts in skill section

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
+// selecting skill container elements
 let boxes = document.querySelectorAll(".skill-item");
 skillContainerRight = document.getElementById('css-right');
 skillContainerLeft = document.getElementById('css-left');
-
 aboutSkillSpan = document.getElementById('aboutSkill');
 skillHeading = document.getElementById('skill-heading');
 skillTextBodyRight = document.getElementById('textBodyRight');
 skillTextBodyLeft = document.getElementById('textBodyLeft');
 nextColumn = document.getElementById('textBodyRightNextCol');
+
+// navbar buttons smoothscroll
+projectsButton = document.getElementById("projects");
+aboutButton = document.getElementById("about");
+contactButton = document.getElementById("contact");
+
+// scroll top button
+goTopButton = document.getElementById("goTopButton");
+
+// adding eventlisteners
 boxes.forEach(elem => {
     elem.addEventListener("click", e => {
       showSkillBody();
@@ -24,7 +34,7 @@ boxes.forEach(elem => {
         javaInfo();
       }
       else if(e.target.closest('.rwd')) {
-        console.log("responsive ")
+        angularInfo();
       }
       else if(e.target.closest('.ai')) {
         console.log("illustrator")
@@ -33,6 +43,7 @@ boxes.forEach(elem => {
     });
 });
 
+// toggle margin for showing section
 let showSkillBody = function() {
   skillContainerRight.style.marginLeft = "20%";
   skillContainerRight.style.opacity = "1";
@@ -40,6 +51,7 @@ let showSkillBody = function() {
   skillContainerLeft.style.opacity = "1";
 }
 
+// html part
 let htmlInfo = function() {
   nextColumn.innerHTML = "";
   skillContainerRight.style.background = "black";
@@ -55,6 +67,7 @@ let htmlInfo = function() {
 
 }
 
+// css part
 let cssInfo = function() {
   nextColumn.innerHTML = "";
   skillContainerRight.style.background = "#000c1e";
@@ -69,7 +82,7 @@ let cssInfo = function() {
   skillTextBodyLeft.innerHTML = "<img src='https://raw.githubusercontent.com/dragance023/portfolio-v1/master/img/css.png'>"
                                + "<h1>CSS</h1>"
 }
-
+// js part
 let jsInfo = function() {
   skillContainerRight.style.background = "#060701";
   skillHeading.innerHTML = "Feeling comfortable with:";
@@ -83,7 +96,7 @@ let jsInfo = function() {
                               + "<h1>Javascript</h1>"
 }
 
-
+// java part
 let javaInfo = function() {
   skillContainerRight.style.background = "#060701";
   skillHeading.innerHTML = "Feeling comfortable with:";
@@ -97,13 +110,25 @@ let javaInfo = function() {
                               + "<h1>Javascript</h1>"
 }
 
+let angularInfo = function() {
+  skillContainerRight.style.background = "black";
+  skillHeading.innerHTML = "Able to work with:";
+  skillTextBodyRight.innerHTML = "<li class='angularText'>Components and Databinding</li>"
+                               + "<li class='angularText'>Services</li>"
+                               + "<li class='angularText'>Dependency Injection</li>"
+                               + "<li class='angularText'>Routing</li>"
+                               + "<li class='angularText'>Observables</li>"
+                               + "<li class='angularText'>Forms</li>"
+                               + "<li class='angularText'>HTTP Requests</li>"
+                               + "<li class='angularText'>TypeScript</li>"
+  aboutSkillSpan.innerHTML     = "Something about JavaCcript"
+  skillTextBodyLeft.innerHTML = "<img src='./img/angular.png'>"
+                              + "<h1>Angular</h1>"
+}
 
 
 
-
-
-
-
+// toggle margin for closing section
 let closeSkillContainer = function() {
   let closeButton = document.getElementById("close");
   closeButton.addEventListener("click", () => {
@@ -111,10 +136,10 @@ let closeSkillContainer = function() {
     skillContainerRight.style.opacity = "0";
     skillContainerLeft.style.marginLeft = "-50%";
     skillContainerLeft.style.opacity = "1";
-
   })
 
 }
+// escape closing
 document.addEventListener('keydown', function(e) {
   if(e.key === "Escape") {
     skillContainerRight.style.marginLeft = "100%";
@@ -123,4 +148,30 @@ document.addEventListener('keydown', function(e) {
     skillContainerLeft.style.opacity = "1";
   }
 })
+
+
+projectsButton.addEventListener("click", () => {
+  window.scroll({
+    top: 850,  
+    behavior: 'smooth'
+  });
+});
+aboutButton.addEventListener("click", () => {
+  window.scroll({
+    top: 2450,  
+    behavior: 'smooth'
+  });
+});
+contactButton.addEventListener("click", () => {
+  window.scroll({
+    top: 2850,  
+    behavior: 'smooth'
+  });
+});
+goTopButton.addEventListener("click", () => {
+  window.scroll({
+    top: 100,  
+    behavior: 'smooth'
+  });
+});
 
